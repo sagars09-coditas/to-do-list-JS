0defcca4426252a7105301952725b3f08a15b424
Sagar Jain <sagar.jain@coditas.in>
2018-01-19 16:59:43 +0530

    Review changes done.

--- To-do-List.js
+++ To-do-List.js
@@ -9,84 +9,66 @@ let addForm = () => {
 let addItem = () => {
     var appendItems = document.getElementById('appendItem');
     appendItems.innerHTML = '';
-
     document.getElementById('form').style.visibility = 'hidden';
     text = document.getElementById('item').value;
     items.push({
         text: text,
         status: "unchecked"
     });
-
-    itemsCopy = items.slice();    
     var toSort = document.getElementById('sortList').options.selectedIndex;
-
     if (toSort == 2) {
-        appendItems.innerHTML = '';
-        let arr = [];
-        let arr1 = [];
-
-        for (i = 0; i< items.length; i++) {
-            if (items[i].status == 'unchecked') {
-                arr.push(i);
+        let completed = [];
+        let incompleted = [];
+        items.forEach(function(index) {
+            if (items[index].status == 'unchecked') {
+                completed.push(index);
             }
-
             else {
-                arr1.push(i);
+                incompleted.push(index);
             }
-        }   
-
-        for (let i = 0; i < arr.length-1; i++) {
-            newList(arr[i]);
-        }
-
+        });
+        completed.forEach(function(index) {
+            newList(completed[index]);
+        });
         newList(items.length - 1);
-        document.getElementById('appendItem').childNodes.item(arr.length - 1).lastChild.className = 'addAnim';
-        
-        for (let i = 0; i < arr1.length; i++) {
+        document.getElementById('appendItem').childNodes.item(completed.length - 1).lastChild.className = 'add-anim';
+        incompleted.forEach(function(index) {
             var div = document.createElement('div');
             var list = document.createElement('li');
             var checkbox = document.createElement('input');
             var p = document.createElement('p');
             var delButton = document.createElement('button');
-        
             checkbox.type = 'checkbox';
-            checkbox.id = `c${arr1[i]}`;
+            checkbox.id = `c${incompleted[index]}`;
             checkbox.addEventListener("click", checked);
-            if (items[arr1[i]].status == "checked") {
+            if (items[incompleted[index]].status == "checked") {
                 checkbox.checked = true;
             }
-        
-            p.id = `p${arr1[i]}`;
-            p.innerText = items[arr1[i]].text;
-        
+            p.id = `p${incompleted[index]}`;
+            p.innerText = items[incompleted[index]].text;
             delButton.type = 'button';
             delButton.className = 'button';
             var del = document.createElement('i');
             del.className = 'fa fa-trash';
             delButton.appendChild(del);
-            delButton.id = `d${arr1[i]}`;
+            delButton.id = `d${incompleted[index]}`;
             delButton.addEventListener("click", deleteItem);
-            
-            list.id = `${arr1[i]}`;
+            list.id = `${incompleted[index]}`;
             list.appendChild(checkbox);
             list.appendChild(p);
             list.appendChild(delButton);
-        
-            div.className = 'downAnim';
+            div.className = 'down-anim';
             div.appendChild(list);
-        
             appendItems.appendChild(div);
-        }        
+        });
     }
-
     else {
-        for (let i = 0; i< items.length; i++) {
+        items.forEach(function(index) {
             newList(i);
             sort();
-        }
-        document.getElementById('appendItem').lastChild.lastChild.className = 'addAnim';
+        });
+        document.getElementById('appendItem').lastChild.lastChild.className = 'add-anim';
     }
-    
     setTimeout(() => {
         sort();
     }, 400);        
@@ -95,86 +77,46 @@ let addItem = () => {
 let sort = () => {
     var appendItems = document.getElementById('appendItem');
     var toSort = document.getElementById('sortList').options.selectedIndex;
-
     if (toSort === 1) {
         appendItems.innerHTML = '';
-
-        for (let i = 0; i< items.length; i++) {
-            if (items[i].status == 'checked') {
-                newList(i);
+        items.forEach(function(index) {
+            if (items[index].status == 'checked') {
+                newList(index);
             }
-        }
-
-        for (let i = 0; i< items.length; i++) {
-            if (items[i].status == 'unchecked') {
-                newList(i);
+        });
+        items.forEach(function(index) {
+            if (items[index].status == 'unchecked') {
+                newList(index);
             }
-        }
+        });
     }
-
     else if (toSort === 2) {
         appendItems.innerHTML = '';
-
-        for (let i = 0; i< items.length; i++) {
-            if (items[i].status == 'unchecked') {
-                newList(i);
+        items.forEach(function(index) {
+            if (items[index].status == 'unchecked') {
+                newList(index);
             }
-        }
-
-        for (let i = 0; i< items.length; i++) {
-            if (items[i].status == 'checked') {
-                newList(i);
+        });
+        items.forEach(function(index) {
+            if (items[index].status == 'checked') {
+                newList(index);
             }
-        }
+        });
     }
-
     else if (toSort === 3) {
         appendItems.innerHTML = '';
-        for (let i = 0; i< itemsCopy.length; i++) {
-            var div = document.createElement('div');
-            var list = document.createElement('li');
-            var checkbox = document.createElement('input');
-            var p = document.createElement('p');
-            var delButton = document.createElement('button');
-
-            checkbox.type = 'checkbox';
-            checkbox.id = `c${i}`;
-            checkbox.addEventListener("click", checked);
-            if (itemsCopy[i].status == "checked") {
-                checkbox.checked = true;
-            }
-
-            p.id = `p${i}`;
-            p.innerText = itemsCopy[i].text;
-
-            delButton.type = 'button';
-            delButton.className = 'button';
-            var del = document.createElement('i');
-            del.className = 'fa fa-trash';
-            delButton.appendChild(del);
-            delButton.id = `d${i}`;
-            delButton.addEventListener("click", deleteItem);
-            
-            list.id = `${i}`;
-            list.appendChild(checkbox);
-            list.appendChild(p);
-            list.appendChild(delButton);
-
-            div.appendChild(list);
-
-            appendItems.appendChild(div);
-        }
+        items.forEach(function(index) {
+            newList(index);
+        });
     }
 };
 
 let deleteItem = (gid) => {
     var rem = document.getElementById(gid.target.id).parentNode;
-    rem.className = 'delAnim';
-
+    rem.className = 'del-anim';
     setTimeout(() => {
         rem.remove();
         items.splice(rem.id, 1);
-        itemsCopy.splice(rem.id, 1);
     }, 250);
 };
 
@@ -182,7 +124,6 @@ let checked = (gid) => {
     var getId = document.getElementById(gid.target.id).parentNode;
     var getChecked = getId.id;
     var checkStatus = items[getChecked].status;
-    
     if (checkStatus == "unchecked") {
         items[getChecked].status = "checked";
         itemsCopy[getChecked].status = "checked";
@@ -190,8 +131,7 @@ let checked = (gid) => {
     else {
         items[getChecked].status = "unchecked";
         itemsCopy[getChecked].status = "unchecked";
-    }
-    
+    }   
     sort();
 };
 
@@ -202,17 +142,14 @@ let newList = (j) => {
     var checkbox = document.createElement('input');
     var p = document.createElement('p');
     var delButton = document.createElement('button');
-
     checkbox.type = 'checkbox';
     checkbox.id = `c${j}`;
     checkbox.addEventListener("click", checked);
     if (items[j].status == "checked") {
         checkbox.checked = true;
     }
-
     p.id = `p${j}`;
     p.innerText = items[j].text;
-
     delButton.type = 'button';
     delButton.className = 'button';
     var del = document.createElement('i');
@@ -220,13 +157,10 @@ let newList = (j) => {
     delButton.appendChild(del);
     delButton.id = `d${j}`;
     delButton.addEventListener("click", deleteItem);
-    
     list.id = `${j}`;
     list.appendChild(checkbox);
     list.appendChild(p);
     list.appendChild(delButton);
-
     div.appendChild(list);
-
     appendItems.appendChild(div);
 };
